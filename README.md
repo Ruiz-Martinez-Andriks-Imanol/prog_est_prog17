9ee9dccddeb07d3b8d7345cb90b6dd8fa7af8123
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-14 18:56:42 -0500

    prog_est_prog17
    Programa que pide la cantidad de números enteros positivos a leer.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # prog_est_prog17
-Programa que pide la cantidad de números enteros positivos a leer
+Autor : Andriks Imanol Ruiz Martínez, Fecha: 24/02/2022 <br>
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa de ciclos y condicionales en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables enteras</li>
+    <li>printf para mostrar varias variables</li>
+    <li>scanf</li>
+    <li>El uso de include para las librerias</li>
+    <li>do-while</li>
+    <li>for</li>
+    <li>Contador</li>
+    <li>Condicionales</li>
+    <li>Comentarios para la documentación interna del programa.</li>
+    </ul>
+    </p>
+<br>
+Objetivo: Programa que pide una cantidad de numeros enteros positivos a leer.
+<br>
+<br>
+<p>Breve descripción:
+Programa que pide una cantidad de numeros enteros positivos a leer y después que introducen todos los números.<br>
+Se calcula lo siguiente:
+<ul>
+<li>Cuantos numeros pares se introdujo</li>
+<li>El número mayor</li>
+<li>El número menor</li>
+<li>Validar que los números sean positivos</li>	
+</ul>
+<br>
+</p>
---
+++ prog17.cpp
@@ -0,0 +1,59 @@
+/* Autor: Andriks Imanol Ruiz Mártinez, Realizado: 24/02/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa
+Materia: Programación Estructrada
+Ciclo: 01/2022
+
+Este es un programa de ciclos y condicionales en Lenguaje C de la materia de Programación Estructurada
+Muestra el uso de:
+
+    -Variables enteras
+    -printf para mostrar varias variables
+    -scanf
+    -El uso de include para las librerias
+    -do-while
+    -for
+    -Contador
+    -Condicionales
+    Comentarios para la documentación interna del programa
+*/
+#include<stdio.h>
+int main() {
+	//Declaraciones
+	int numeros, dato, pares=0, mayor, menor;
+	//Procesos
+	//Introducir cuantos numeros va a leer
+	printf("Introduce la cantidad de numeros a leer: ");
+	scanf("%d",&numeros);
+	for(int i=1;i<=numeros;i++){
+		//Pedir el dato y validar que sea positivo
+		do {
+			printf("Introudce el numero %d (positivo): ",i);
+			scanf("%d",&dato);
+			if(dato<=0){
+				printf("Numero negativo, intenta nuevamente.\n");
+			}
+		}while(dato<=0); //condicion de un dato invalido, por lo tanto lo vuelve a pedir
+		//probar si es par
+		if (dato%2==0){ //Si un numero es multiplo de 2, es decir su residuo es 0
+			pares++;
+		}
+		if(i==1){ //el primer dato no tiene con quien compararse, automaticamente es mayor y menor
+			menor=dato;
+			mayor=dato;
+		}
+		else {
+			//Los demas ya se comparan con los anteriores valores introducidos
+			if (dato>mayor){
+				mayor=dato;
+			}
+			if (dato<menor){
+				menor=dato;
+			}
+		}
+	}
+	//Salida
+	printf("La cantidad de numeros pares es: %d\n",pares);
+	printf("El numero mayor es: %d\n",mayor);
+	printf("El numero menor es: %d\n",menor);
+	return 0;	
+}
